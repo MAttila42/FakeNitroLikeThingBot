ea919bec3fdd9fe6131f904cdb472f0ca78ae3ce
ExAtom <menyhart.atika@gmail.com>
2021-02-22 14:46:48 +0100

    Better Help command

--- Commands/Commands.cs
+++ Commands/Commands.cs
@@ -56,34 +56,24 @@ namespace FNLTB.Commands
             commands.Add(new Command("Test", Test.Aliases, Test.Description, Test.Usages, Test.Permission));
 
             var settings = Settings.PullData();
-            string content = "";
+            var embed = new EmbedBuilder()
+                .WithAuthor(author => { author.WithIconUrl("https://cdn.discordapp.com/attachments/782305154342322226/801852346350174208/noun_Information_405516.png"); }) // Information by Viktor Ostrovsky from the Noun Project
+                .WithFooter("Made by ExAtom")
+                .WithColor(new Color(0x7289DA));
             switch (m.Length)
             {
                 case 1:
-                    content += $"For more information use `{settings[settings.IndexOf(settings.Find(x => x.ServerID == ((SocketGuildChannel)message.Channel).Guild.Id))].Prefix}commands <command>`.";
+                    embed.WithAuthor(author => { author.WithName("Commands"); });
+                    embed.WithDescription($"For more information use `{settings[settings.IndexOf(settings.Find(x => x.ServerID == ((SocketGuildChannel)message.Channel).Guild.Id))].Prefix}commands <command>`.\nSource code on [Github](https://github.com/ExAtom/FakeNitroLikeThingBot).\nPing the bot for other help.");
                     foreach (var command in commands)
-                        content += $"\n\n**{command.Name}**\n*{command.Description}*";
-                    content += "\n\nSource code on [Github](https://github.com/ExAtom/FakeNitroLikeThingBot).";
-
-                    var embed = new EmbedBuilder()
-                        .WithAuthor(author =>
-                        {
-                            author
-                                .WithName("Commands")
-                                .WithIconUrl("https://cdn.discordapp.com/attachments/782305154342322226/801852346350174208/noun_Information_405516.png"); // Information by Viktor Ostrovsky from the Noun Project
-                        })
-                        .WithDescription(content)
-                        .WithFooter(((SocketGuildChannel)message.Channel).Guild.Name)
-                        .WithColor(new Color(0x7289DA)).Build();
-                    try { await message.Channel.SendMessageAsync(null, embed: embed); }
-                    catch (Exception) { }
+                        embed.AddField(command.Name, $"*{command.Description}*");
                     break;
                 case 2:
                     var foundCommands = commands.Where(x => x.Aliases.Contains(m[1].ToLower())).ToList();
 
                     foreach (var command in foundCommands)
                     {
-                        content = $"{command.Description}\n" +
+                        string content = $"{command.Description}\n" +
                             $"{command.Permission}\n\n" +
                             $"Usage{(command.Usages.Length > 1 ? "s:\n" : ": ")}";
                         foreach (var usage in command.Usages)
@@ -92,18 +82,8 @@ namespace FNLTB.Commands
                         for (int j = 0; j < command.Aliases.Count(); j++)
                             content += $"`{command.Aliases[j]}`{(j < command.Aliases.Count() - 1 ? ", " : "\n\n")}";
 
-                        var embed2 = new EmbedBuilder()
-                            .WithAuthor(author =>
-                            {
-                                author
-                                    .WithName(command.Name)
-                                    .WithIconUrl("https://cdn.discordapp.com/attachments/782305154342322226/801852346350174208/noun_Information_405516.png"); // Information by Viktor Ostrovsky from the Noun Project
-                            })
-                            .WithDescription(content)
-                            .WithFooter(((SocketGuildChannel)message.Channel).Guild.Name)
-                            .WithColor(new Color(0x7289DA)).Build();
-                        try { await message.Channel.SendMessageAsync(null, embed: embed2); }
-                        catch (Exception) { }
+                        embed.WithAuthor(author => { author.WithName(command.Name); })
+                            .WithDescription(content);
                     }
                     break;
 
@@ -111,6 +91,8 @@ namespace FNLTB.Commands
                     await message.Channel.SendMessageAsync("❌ Too many parameters!");
                     return;
             }
+            try { await message.Channel.SendMessageAsync(null, embed: embed.Build()); }
+            catch (Exception) { }
         }
     }
 }
